f736175dafea287692adefcbbcf0f90ed9a59c47
Usuariozombie <edupime_98@hotmail.com>
2022-11-18 19:04:47 +0100

    JS and CSS minor changes

--- dist/script.js
+++ dist/script.js
@@ -14,7 +14,61 @@ $(document).ready(function () {
   })(jQuery);
 
   // input text for typing animation
-  $("#holder").writeText("WEB DESIGNER + FRONT-END DEVELOPER");
+  var _CONTENT = [ "HEAR THE MUSIC", "FEEL THE MUSIC", "DISCOVER THE WORLD" ];
+  
+  // Current sentence being processed
+  var _PART = 0;
+  
+  // Character number of the current sentence being processed 
+  var _PART_INDEX = 0;
+  
+  // Holds the handle returned from setInterval
+  var _INTERVAL_VAL;
+  
+  // Element that holds the text
+  var _ELEMENT = document.querySelector("#text");
+  
+  // Implements typing effect
+  function Type() { 
+    var text =  _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+    _ELEMENT.innerHTML = text;
+    _PART_INDEX++;
+  
+    // If full sentence has been displayed then start to delete the sentence after some time
+    if(text === _CONTENT[_PART]) {
+      clearInterval(_INTERVAL_VAL);
+      setTimeout(function() {
+        _INTERVAL_VAL = setInterval(Delete, 50);
+      }, 1000);
+    }
+  }
+  
+  // Implements deleting effect
+  function Delete() {
+    var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+    _ELEMENT.innerHTML = text;
+    _PART_INDEX--;
+  
+    // If sentence has been deleted then start to display the next sentence
+    if(text === '') {
+      clearInterval(_INTERVAL_VAL);
+  
+      // If last sentence then display the first one, else move to the next
+      if(_PART == (_CONTENT.length - 1))
+        _PART = 0;
+      else
+        _PART++;
+      _PART_INDEX = 0;
+  
+      // Start to display the next sentence after some time
+      setTimeout(function() {
+        _INTERVAL_VAL = setInterval(Type, 100);
+      }, 200);
+    }
+  }
+  
+  // Start the typing effect on load
+  _INTERVAL_VAL = setInterval(Type, 100);
 
   // initialize wow.js
   new WOW().init();
@@ -99,7 +153,7 @@ $(document).ready(function () {
         $(".header-links").css("background-color", "transparent");
       } else if (index != 1) {
         $(".header-links a").each(function () {
-          $(this).css("color", "black");
+          $(this).css("color", "white");
         });
       }
 
